21b4b8d70c5ad8355e2e3931d03fe9280d808608
Ankit Singh <94107791+ankits6270@users.noreply.github.com>
2025-01-16 22:28:32 +0530

    setting up connection string

--- db/connect.js
+++ db/connect.js
@@ -1,1 +1,5 @@
+const mongoose = require('mongoose');
+
 const connectionString = "mongodb+srv://ankits6270:1234@cluster0.axnhicp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+mongoose.connect(connectionString);
